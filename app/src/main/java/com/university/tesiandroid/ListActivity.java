99f37bbbaf6a78a2d0b8a3114a4421e01991d9d6
Zhrack <davi91>
2016-04-02 15:47:17 +0200

    GPS management done.

--- app/src/main/java/com/university/tesiandroid/ListActivity.java
+++ app/src/main/java/com/university/tesiandroid/ListActivity.java
@@ -1,12 +1,14 @@
 package com.university.tesiandroid;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -14,25 +16,65 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class ListActivity extends Activity {
+public class ListActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    // LogCat tag
+    private static final String TAG = ListActivity.class.getSimpleName();
+
+    // GPS STUFF
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+
+    private Location mLastLocation;
+    private double latitude;
+    private double longitude;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+
+    // boolean flag to toggle periodic location updates
+    private boolean mRequestingLocationUpdates = false;
+
+    private LocationRequest mLocationRequest;
+
+    // Location updates intervals in sec
+    private static int UPDATE_INTERVAL = 10000; // 10 sec
+    private static int FATEST_INTERVAL = 5000; // 5 sec
+    private static int DISPLACEMENT = 5; // 1 meters
+
+    // UI elements
 
     private ListView listView;
-
     private ListAdapter adapter;
+
+    private Button toggleGPSbtn;
+    private TextView txtLatitude;
+    private TextView txtLongitude;
+
 
     private ArrayList<PointInfo> points = new ArrayList<>();
 
     // Receives list requests from server
     private JsonObject jsonResponse;
+
+    // Search radius to be sent to server
+    private float searchRadius = 10000; // in meters
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +82,9 @@ public class ListActivity extends Activity {
         setContentView(R.layout.activity_list);
 
         listView = (ListView) findViewById(R.id.list_home);
+        toggleGPSbtn = (Button) findViewById(R.id.toggleGPS_btn);
+        txtLatitude = (TextView) findViewById(R.id.txt_latitude);
+        txtLongitude = (TextView) findViewById(R.id.txt_longitude);
 
         adapter = new ListAdapter(points, getApplicationContext());
         // Assign adapter to ListView
@@ -65,15 +110,55 @@ public class ListActivity extends Activity {
 
         });
 
+        toggleGPSbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                togglePeriodicLocationUpdates();
+            }
+        });
+
         AppController.setCtx(this);
+
+        // First we need to check availability of play services
+        if (checkPlayServices()) {
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+
+            createLocationRequest();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("Test", "onResume");
-        // get books from server
-        askPoints();
+        Log.d(TAG, "onResume");
+
+        checkPlayServices();
+
+        // Resuming the periodic location updates
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+        //askPoints();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();  // Always call the superclass method first
+
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Once connected with google api, get the location
+        getLocation();
+
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+        // get points from server
+//        askPoints();
     }
 
     public void askPoints()
@@ -106,7 +191,11 @@ public class ListActivity extends Activity {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("option", "bookList");
+                params.put("option", "getPointList");
+                Log.d(TAG, "sending latitude " + String.valueOf(latitude));
+                Log.d(TAG, "sending longitude " + String.valueOf(longitude));
+                params.put("latitude", String.valueOf(latitude));
+                params.put("longitude", String.valueOf(longitude));
 
                 return params;
             }
@@ -116,4 +205,152 @@ public class ListActivity extends Activity {
         AppController.getInstance(this).addToRequestQueue(jsonObjReq,
                 "Volley");
     }
+
+    /**
+     * Method to display the location on UI
+     * */
+    private void getLocation() {
+
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+        }
+        else{
+            Log.d(TAG, "(Couldn't get the location. Make sure location is enabled on the device)");
+        }
+    }
+
+    /**
+     * Method to toggle periodic location updates
+     * */
+    private void togglePeriodicLocationUpdates() {
+        if (!mRequestingLocationUpdates) {
+            mRequestingLocationUpdates = true;
+            toggleGPSbtn.setText(R.string.txt_toggleStop);
+
+            // Starting the location updates
+            startLocationUpdates();
+
+            Log.d(TAG, "Periodic location updates started!");
+
+        } else {
+            mRequestingLocationUpdates = false;
+            toggleGPSbtn.setText(R.string.txt_toggleStart);
+
+            // Stopping the location updates
+            stopLocationUpdates();
+
+            Log.d(TAG, "Periodic location updates stopped!");
+        }
+    }
+
+    /**
+     * Creating location request object
+     * */
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    /**
+     * Starting the location updates
+     * */
+    protected void startLocationUpdates() {
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+        Log.d(TAG, "latitude " + String.valueOf(latitude));
+        Log.d(TAG, "longitude " + String.valueOf(longitude));
+
+    }
+
+    /**
+     * Stopping location updates
+     */
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    /**
+     * Method to verify google play services on the device
+     * */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Creating google api client object
+     * */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Assign the new location
+        mLastLocation = location;
+
+        Toast.makeText(getApplicationContext(), "Location changed!",
+                Toast.LENGTH_SHORT).show();
+
+        // Displaying the new location on UI
+        getLocation();
+
+        txtLatitude.setText("Lat: " + String.valueOf(latitude));
+        txtLongitude.setText("Long: " + String.valueOf(longitude));
+
+    }
+
 }
